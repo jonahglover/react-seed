5acda8f95b3ced80ef4fae16e75e2158b3aa181f
Jonah Glover <jonah@luxe.com>
2017-06-04 00:18:19 -0700

    refactor: remove some dead code and refactor to use a different component structure

---
+++ src/components/App/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="title-text">
+        TEXT
+      </div>
+      <div className="subtext">
+        Subtext
+      </div>
+    </div>
+  );
+}
+
+export default App;
